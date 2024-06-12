65f5a00166d8828352410d50f26f5e289fd39231
KIMTAESEONG <aka404365@gmail.com>
2024-06-12 19:59:36 +0900

    feat: edit emoji

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,11 +52,11 @@ export default function Home() {
       </div>
       <div className="flex justify-center">
         <div className="grid grid-cols-3 justify-items-center items-center gap-[20px]">
-          <PostListCard href="/community" lable="📋 커뮤니티" data={jobData} />
-          <PostListCard href="/business" lable="🚴🏻 업소록" data={jobData} />
-          <PostListCard href="/job" lable="📰 구인/구직" data={jobData} />
-          <PostListCard href="/market" lable="📦 쿼카마켓" data={jobData} />
-          <PostListCard href="/rentshare" lable="📢 렌트/쉐어" data={jobData} />
+          <PostListCard href="/community" lable="🏙️ 커뮤니티" data={jobData} />
+          <PostListCard href="/business" lable="🏘️ 업소록" data={jobData} />
+          <PostListCard href="/job" lable="🙋🏻 구인/구직" data={jobData} />
+          <PostListCard href="/market" lable="🍎 쿼카마켓" data={jobData} />
+          <PostListCard href="/rentshare" lable="🚗 렌트/쉐어" data={jobData} />
         </div>
       </div>
     </div>
