2f41c5c1982db659a12bdcffa4739ab724f3dc29
KIMTAESEONG <aka404365@gmail.com>
2024-10-15 16:12:50 +0900

    feat: 달력 스타일링

--- src/app/_components/CalendarWidget/index.tsx
+++ src/app/_components/CalendarWidget/index.tsx
@@ -56,102 +56,74 @@ const CalendarWrapper = styled.div`
 
   .react-calendar {
     border-radius: 16px;
-    padding: 21px 26px 40px 26px;
+    padding: 21px 16px 26px 16px;
     background-color: white;
     border: 1px solid #dadada;
   }
 
-  /* 전체 폰트 컬러 */
-  .react-calendar__month-view {
-    abbr {
-      /* color: ${(props) => props.theme.gray_1}; */
-    }
-  }
-
-  /* 네비게이션 가운데 정렬 */
   .react-calendar__navigation {
-    /* justify-content: center; */
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 8px;
   }
 
-  /* 네비게이션 폰트 설정 */
-  .react-calendar__navigation button {
-    /* font-weight: 800; */
-    /* font-size: 1rem; */
-  }
-
-  /* 네비게이션 버튼 컬러 */
-  .react-calendar__navigation button:focus {
-    /* background-color: white; */
-  }
-
-  /* 네비게이션 비활성화 됐을때 스타일 */
-  .react-calendar__navigation button:disabled {
-    /* background-color: white;
-    color: ${(props) => props.theme.darkBlack}; */
-  }
-
-  /* 년/월 상단 네비게이션 칸 크기 줄이기 */
   .react-calendar__navigation__label {
-    /* flex-grow: 0 !important; */
+    flex: 1;
+    font-family: SUIT;
+    font-size: 16px;
+    font-weight: 800;
+    line-height: 19.97px;
+    letter-spacing: 0.48356959223747253px;
+    text-align: center;
+    color: #222222;
   }
 
-  /* 요일 밑줄 제거 */
+  .react-calendar__navigation__arrow {
+    all: unset;
+    background: #eef2ff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 18px;
+    height: 18px;
+    border-radius: 9.2px;
+  }
+
+  .react-calendar__month-view__weekdays__weekday {
+    all: unset;
+    font-family: Nunito;
+    font-size: 11px;
+    font-weight: 600;
+    line-height: 15.48px;
+    text-align: center;
+    color: #1568c3;
+    margin-bottom: 19px;
+  }
+
+  .react-calendar__month-view__days__day {
+    all: unset;
+    font-family: Nunito;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 16.37px;
+    text-align: center;
+    color: #7c86a2;
+    margin-bottom: 12px;
+  }
+  .react-calendar__month-view__days__day--neighboringMonth {
+    all: unset;
+    font-family: Nunito;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 16.37px;
+    text-align: center;
+    color: #cbcbcb;
+    margin-bottom: 12px;
+  }
+
   .react-calendar__month-view__weekdays abbr {
     text-decoration: none;
     font-weight: 800;
-  }
-
-  /* 일요일에만 빨간 폰트 */
-  .react-calendar__month-view__weekdays__weekday--weekend abbr[title='일요일'] {
-    color: ${(props) => props.theme.red_1};
-  }
-
-  /* 오늘 날짜 폰트 컬러 */
-  .react-calendar__tile--now {
-    background: none;
-    abbr {
-      color: ${(props) => props.theme.primary_2};
-    }
-  }
-
-  /* 네비게이션 월 스타일 적용 */
-  .react-calendar__year-view__months__month {
-    /* border-radius: 0.8rem;
-    background-color: ${(props) => props.theme.gray_5};
-    padding: 0; */
-  }
-
-  /* 네비게이션 현재 월 스타일 적용 */
-  .react-calendar__tile--hasActive {
-    background-color: ${(props) => props.theme.primary_2};
-    abbr {
-      /* color: white; */
-    }
-  }
-
-  /* 일 날짜 간격 */
-  .react-calendar__tile {
-    /* padding: 5px 0px 18px;
-    position: relative; */
-  }
-
-  /* 네비게이션 월 스타일 적용 */
-  .react-calendar__year-view__months__month {
-    /* flex: 0 0 calc(33.3333% - 10px) !important;
-    margin-inline-start: 5px !important;
-    margin-inline-end: 5px !important;
-    margin-block-end: 10px;
-    padding: 20px 6.6667px;
-    font-size: 0.9rem;
-    font-weight: 600;
-    color: ${(props) => props.theme.gray_1}; */
-  }
-
-  /* 선택한 날짜 스타일 적용 */
-  .react-calendar__tile:enabled:hover,
-  .react-calendar__tile:enabled:focus,
-  .react-calendar__tile--active {
-    /* background-color: ${(props) => props.theme.yellow_2};
-    border-radius: 0.3rem; */
   }
 `
