89ee233f5bb561f386bad4443b3106bcbd04eda5
KIMTAESEONG <aka404365@gmail.com>
2025-03-02 00:56:33 +0900

    feat: 프로필 설정 폼 구현

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,116 +2,107 @@
 
 import styled from 'styled-components'
 import EmptyImage from '@/assets/empty_profile.svg'
-import { useForm } from 'react-hook-form'
-import { useRef } from 'react'
-import SettingInput from './SettingInput'
-import ChangePassword from './ChangePassword'
+import useUser from '@/hooks/useUser'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
-interface SettingsForm {
+type UserProfile = {
+  profile_image: string | null
   email: string
   nickname: string
-  profileImage: string | null
-  password: string
-  passwordCheck: string
+  description: string | null
 }
 
 function Settings() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<SettingsForm>({
-    mode: 'onBlur',
-    defaultValues: {
-      email: 'aka404365@gmail.com',
-      nickname: '김태성',
-      password: '비밀번호 변경은 이메일 로그인만 가능합니다.',
-    },
-    // defaultValues: { email: 'aka404365@gmail.com', nickname: '김태성' } || {},
-    // ToDo: DefaultValue 받아오기
+  const { user } = useUser()
+  const [userProfile, setUserProfile] = useState<UserProfile>({
+    profile_image: '',
+    email: '',
+    nickname: '',
+    description: '',
   })
-  const passwordRef = useRef<string>('')
-  passwordRef.current = watch('password')
+  // React의 useState는 초기값만 한 번 설정될 뿐, 이후에는 자동으로 업데이트되지 않는다
+  // 이후 user 값이 들어와도, useState는 userProfile을 자동으로 업데이트하지 않음. useState는 초기 렌더링 시 지정된 값만 기억하고 유지
 
-  //   ToDo userProvider에 따라 비밀번호 변경 버튼 만든 후, disabled
-
-  const onValid = async (formData: SettingsForm) => {
-    console.log('formData', formData)
-    // if provider === email 슈퍼베이스 업데이트
-    // if (formData.password === '비밀번호 변경은 이메일 로그인만 가능합니다.') {
-    //   delete formData.password
-    //   delete formData.passwordCheck
-    // }
-    console.log('formData', formData)
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      const imageUrl = URL.createObjectURL(file) // 선택한 이미지의 URL 생성
+      setUserProfile((prev) => ({
+        ...prev,
+        profile_image: imageUrl,
+      }))
+    }
   }
+
+  useEffect(() => {
+    if (user) {
+      setUserProfile({
+        profile_image: user.profile_image,
+        email: user.email,
+        nickname: user.nickname,
+        description: user.description,
+      })
+    }
+  }, [user])
 
   return (
     <Container>
       <h3>프로필 수정</h3>
       <div>
         <ImageEdit>
-          <EmptyImage />
-          프로필 사진 변경
-          <label htmlFor="profileImage">
-            파일 선택
-            <input id="profileImage" type="file" />
-          </label>
-          프로필 사진 삭제
-        </ImageEdit>
-        <DetailEdit onSubmit={handleSubmit(onValid)}>
-          {/* 이메일 Default Value 추가 */}
-          <SettingInput label="이메일" disabled register={register('email')} />
-          <SettingInput
-            label="닉네임"
-            register={register('nickname', {
-              required: '닉네임을 입력해 주세요.',
-              //   validate: async (value) => {
-              //     return (
-              //       (await checkExists(value, 'nickname')) ||
-              //       '이미 존재하는 닉네임입니다.'
-              //     )
-              //   },
-            })}
-            errorMessage={errors.nickname?.message || ''}
-          />
-          {false && (
-            <Container>
-              <SettingInput
-                label="비밀번호 변경"
-                type="password"
-                register={register('password', {
-                  required: '비밀번호는 필수 입력 사항입니다.',
-                  minLength: {
-                    value: 6,
-                    message: '비밀번호는 최소 6자 이상이어야 합니다.',
-                  },
-                  maxLength: {
-                    value: 15,
-                    message: '비밀번호는 최대 15자 이하이어야 합니다.',
-                  },
-                  validate: {
-                    hasLetter: (value) =>
-                      /[a-zA-Z]/.test(value) || '영문자가 하나 이상 포함되어야 합니다.',
-                    hasNumber: (value) => /\d/.test(value) || '숫자가 하나 이상 포함되어야 합니다.',
-                  },
-                })}
-                errorMessage={errors.password?.message || ''}
-              />
-              <SettingInput
-                type="password"
-                register={register('passwordCheck', {
-                  required: '비밀번호를 한번 더 입력해 주세요.',
-                  validate: (passwordCheck) =>
-                    passwordCheck === passwordRef.current || '비밀번호가 일치하지 않습니다.',
-                })}
-                errorMessage={errors.passwordCheck?.message || ''}
-              />
-            </Container>
+          {userProfile?.profile_image ? (
+            <Image
+              src={userProfile?.profile_image}
+              alt={`${userProfile.nickname}'s profile image`}
+              width={300}
+              height={300}
+            />
+          ) : (
+            <EmptyImage />
           )}
+          <h3>프로필 사진 변경</h3>
+          <ProfileSelect>
+            <label htmlFor="profileImage">
+              파일 선택
+              <input id="profileImage" type="file" accept="image/*" onChange={handleImageChange} />
+            </label>
+          </ProfileSelect>
+          <ImageDeleteCheckbox>
+            <label htmlFor="basic-checkbox">
+              <input type="checkbox" id="basic-checkbox" />
+              <span>프로필 사진 비우기</span>
+            </label>
+          </ImageDeleteCheckbox>
+        </ImageEdit>
+        <DetailEdit>
+          <SettingInput>
+            <p>이메일</p>
+            <input type="text" value={userProfile.email} disabled />
+          </SettingInput>
+          <SettingInput>
+            <p>닉네임</p>
+            <input
+              type="text"
+              value={userProfile.nickname}
+              onChange={(e) => {
+                setUserProfile({ ...userProfile, nickname: e.target.value })
+              }}
+            />
+          </SettingInput>
+          <SettingInput>
+            <p>한 줄 소개</p>
+            <input
+              type="text"
+              value={userProfile.description || ''}
+              onChange={(e) => {
+                setUserProfile({ ...userProfile, description: e.target.value })
+              }}
+            />
+          </SettingInput>
           <Controls>
             <button type="submit">저장</button>
-            <button>캔버라커넥트 탈퇴</button>
+            <button type="button">캔버라커넥트 탈퇴</button>
           </Controls>
         </DetailEdit>
       </div>
@@ -146,10 +137,57 @@ const ImageEdit = styled.div`
   flex-direction: column;
   justify-content: center;
 
+  img {
+    object-fit: cover;
+  }
+
   svg {
     width: 300px;
     height: 300px;
     background-color: #f0f0f0;
+  }
+
+  & > h3 {
+    margin: 40px 0 0 0;
+  }
+
+  & > div {
+  }
+`
+
+const ProfileSelect = styled.div`
+  margin-top: 5px;
+  display: flex;
+
+  label {
+    border-radius: 8px;
+    padding: 0px 10px;
+    color: white;
+    background-color: #787880;
+
+    &:hover {
+      opacity: 0.8;
+      cursor: pointer;
+    }
+
+    & > input {
+      display: none;
+    }
+  }
+`
+
+const ImageDeleteCheckbox = styled.div`
+  margin-top: 10px;
+
+  & > label {
+    display: flex;
+    align-items: center;
+
+    input {
+      margin: 0 5px 0 0;
+      width: 16px;
+      height: 16px;
+    }
   }
 `
 
@@ -162,7 +200,7 @@ const DetailEdit = styled.div`
 const Controls = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 50px 0 30px 0;
+  padding: 0;
 
   button {
     all: unset;
@@ -171,5 +209,40 @@ const Controls = styled.div`
     background-color: #007aff;
     font-weight: 600;
     color: white;
+  }
+`
+const SettingInput = styled.div`
+  margin-bottom: 16px;
+
+  p {
+    margin: 0 0 4px 0;
+    font-size: 16px;
+    font-weight: 700;
+  }
+
+  input {
+    border: 1px solid rgba(60, 60, 67, 0.15);
+    padding: 5px;
+    width: 360px;
+    font-size: 17px;
+    font-weight: 400;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+    border-radius: 6px;
+    &::placeholder {
+      color: rgba(60, 60, 67, 0.3);
+    }
+
+    &:hover {
+      border-color: #3399ff;
+    }
+
+    &:focus {
+      border-color: #3399ff;
+      box-shadow: 0 0 0 3px #b6daff;
+    }
+
+    &:focus-visible {
+      outline: 0;
+    }
   }
 `
