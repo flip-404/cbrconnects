04978913949b25817d4b9b9b466acad95a149bb5
KIMTAESEONG <aka404365@gmail.com>
2025-04-14 01:51:47 +0900

    fix build err

--- src/components/home/mobile/AnnounceSection.tsx
+++ src/components/home/mobile/AnnounceSection.tsx
@@ -1,10 +1,15 @@
+import { headers } from 'next/headers'
 import AnnounceBox from './AnnounceBox'
 
 // 이거 포스트
 export default async function AnnounceSection() {
+  const headersList = headers()
+  const host = headersList.get('host') || ''
+  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https'
+  const res = await fetch(`${protocol}://${host}/api/recent`)
   const [noticeRes, promotionRes] = await Promise.all([
-    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/posts?category=NOTICE`),
-    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/posts?category=PROMOTION`),
+    fetch(`${protocol}://${host}/api/posts?category=NOTICE`),
+    fetch(`${protocol}://${host}/api/posts?category=PROMOTION`),
   ])
 
   const [noticeData, promotionData] = await Promise.all([noticeRes.json(), promotionRes.json()])
